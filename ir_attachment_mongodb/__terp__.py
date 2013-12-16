e60400f5bfaa5cfd3d5af59ca603ee4492a540bc
Eduard Carreras <ecarreras@gisce.net>
2013-12-16 18:42:57 +0100

    Bump to v0.1.0

--- ir_attachment_mongodb/__terp__.py
+++ ir_attachment_mongodb/__terp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "ir_attachment_mongodb",
-    "version": "0.1.0-beta",
+    "version": "0.1.0",
     "depends": ["base", "mongodb_backend"],
     "author": "GISCE-TI, S.L",
     "category": "Base",
